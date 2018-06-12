130f555359299bf1e66660e81988e948d3454836
Nicolas Léveillé <nicolas@uucidl.com>
2018-06-12 17:17:22 +0200

    Task scheduler

--- emacs/ion-mode.el
+++ emacs/ion-mode.el
@@ -1,40 +1,86 @@
-;;
+;; ion-mode.el -- major mode for editing ion files
+
+;; @url: http://cc-mode.sourceforge.net/derived-mode-ex.el
 ;; @url: http://www.wilfred.me.uk/blog/2015/03/19/adding-a-new-language-to-emacs/
 ;; @url: http://www.davidchristiansen.dk/2014/07/16/implementing-an-emacs-programming-language-mode-beyond-the-basics/
 ;; @url: https://www.emacswiki.org/emacs/ProgMode
 ;;
 
-(define-derived-mode ion-mode prog-mode
-  "ion"
-  :syntax-table ion-mode-syntax-table
-  (font-lock-fontify-buffer))
+(require 'cc-mode)
 
-(defconst ion-mode-syntax-table
-  (let ((table (make-syntax-table)))
-    (modify-syntax-entry ?_ "_" table)
-    (modify-syntax-entry ?\\ "\\" table)
-    (modify-syntax-entry ?+ "." table)
-    (modify-syntax-entry ?- "." table)
-    (modify-syntax-entry ?+ "." table)
-    (modify-syntax-entry ?= "." table)
-    (modify-syntax-entry ?% "." table)
-    (modify-syntax-entry ?< "." table)
-    (modify-syntax-entry ?& "." table)
-    (modify-syntax-entry ?| "." table)
-    (modify-syntax-entry ?\' "\""    table)
-    (modify-syntax-entry ?\240 "."   table)
+(eval-when-compile
+  (require 'cc-langs)
+  (require 'cc-fonts)
+  (c-add-language 'ion-mode 'c-mode))
 
-    ;; comments
-    (modify-syntax-entry ?/ ". 124b" table)
-    (modify-syntax-entry ?* ". 23" table)
+(c-lang-defconst c-modifier-kwds
+  ion (append '("func" "var" "const") (c-lang-const c-modifier-kwds)))
 
-    ;; newlines
-    (modify-syntax-entry ?\n "> b" table)
-    (modify-syntax-entry ?\^m "> b" table)
-			 
-    ;; identifiers
-    (modify-syntax-entry ?_ "w" table)
-    (modify-syntax-entry ?$ "w" table)
+(c-lang-defconst c-cpp-matchers ion nil)
 
-    table))
+(defcustom ion-font-lock-extra-types nil
+  "*List of extra types (aside from the type keywords) to recognize in ion mode. Each list item should be a regexp matching a single identifier.")
 
+(defcustom ion-font-lock-keywords-1 (c-lang-const c-matchers-1 ion)
+  "Minimal highlighting for ion mode")
+
+(defcustom ion-font-lock-keywords-2 (c-lang-const c-matchers-2 ion)
+  "Fast normal highlighting for ion mode")
+
+(defcustom ion-font-lock-keywords-3 (c-lang-const c-matchers-2 ion)
+  "Accurate normal highlighting for ion mode.")
+
+(defvar ion-font-lock-keywords ion-font-lock-keywords-3
+  "Default expressions to highlight in ion mode.")
+
+(defvar ion-mode-syntax-table nil
+  "Syntax table used in ion-mode buffers.")
+
+(or ion-mode-syntax-table
+    (setq ion-mode-syntax-table
+	  (funcall (c-lang-const c-make-mode-syntax-table ion))))
+
+(defvar ion-mode-abbrev-table nil
+  "Abbreviation table used in ion-mode buffers.")
+
+(c-define-abbrev-table 'ion-mode-abbrev-table
+  '(("else" "else" c-electric-continued-statement 0)
+    ("while" "while" c-electric-continued-statement 0)))
+
+(defvar ion-mode-map (let ((map (c-make-inherited-keymap))) map)
+  "Keymap used in ion-mode buffers.")
+
+(easy-menu-define ion-menu ion-mode-map "Ion Mode Commands"
+  (cons "ion" (c-lang-const c-mode-menu ion)))
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.ion\\'" . ion-mode))
+
+;;;###autoload
+(defun ion-mode ()
+  "Major mode for editing ion code. Derived from CC Mode.
+
+The hook `c-mode-common-hook' is run with no args at mode
+initialization, then `ion-mode-hook'.
+
+Key bindings:
+\\{ion-mode-map}"
+  (interactive)
+  (kill-all-local-variables)
+  (c-initialize-cc-mode t)
+  (set-syntax-table ion-mode-syntax-table)
+  (setq major-mode 'ion-mode
+	mode-name "ion"
+	local-abbrev-table ion-mode-abbrev-table
+	abbrev-mode t)
+  (use-local-map ion-mode-map)
+  (c-init-language-vars ion-mode)
+  (c-common-init 'ion-mode)
+  (easy-menu-add ion-menu)
+  (run-hooks 'c-mode-common-hook)
+  (run-hooks 'ion-mode-hook)
+  (c-update-modeline))
+
+(provide 'ion-mode)
+
+;; ion-mode ends here.
