cc039cf82a74415d99da5e50cba1f1b0b193333f
nicolas <nicolas@uucidl.com>
2018-07-05 10:36:22 +0200

    First semi-working dir listing on win32

--- tests_win32.bat
+++ tests_win32.bat
@@ -1,14 +1,15 @@
+@echo off
 setlocal
 set CLFlags=%CLFLAGS%
 
 ion filesystem_tests || exit /b 1
 set O="filesystem_tests.exe"
-cl out_filesystem_tests.c /Fe:"%O%" %CLFlags% || echo /b 1
+cl out_filesystem_tests.c /Fe:"%O%" %CLFlags% -nologo || echo /b 1
 echo PROGRAM	%O%	TEST
 
 ion tasks_tests || exit /b 1
 set O="tasks_tests.exe"
-cl out_tasks_tests.c /Fe:"%O%" %CLFlags% || echo /b 1
+cl out_tasks_tests.c /Fe:"%O%" %CLFlags% -nologo || echo /b 1
 echo PROGRAM	%O%	TEST
 
 
