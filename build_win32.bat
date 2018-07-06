83595c64d7a57673cb328f75dffd18d51ae5b5b7
nicolas <nicolas@uucidl.com>
2018-07-06 10:20:47 +0200

    WIP:tasks

--- build_win32.bat
+++ build_win32.bat
@@ -4,6 +4,6 @@ set CLFlags=%CLFLAGS%
 ion paltool || exit /b 1
 
 set O="paltool.exe"
-cl out_paltool.c /Fe:"%{O}%" %CLFlags% || echo /b 1
+cl out_paltool.c /Fe:"%O%" %CLFlags% || echo /b 1
 echo PROGRAM	%O%
 
