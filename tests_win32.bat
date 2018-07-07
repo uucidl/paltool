301ba41b397494c4ce7b34120db6e80686528630
nicolas <nicolas@uucidl.com>
2018-07-07 11:02:22 +0200

    rearrange the onion

--- tests_win32.bat
+++ tests_win32.bat
@@ -2,14 +2,23 @@
 setlocal
 set CLFlags=%CLFLAGS%
 
-ion filesystem_tests || exit /b 1
-set O="filesystem_tests.exe"
-cl out_filesystem_tests.c /Fe:"%O%" %CLFlags% -nologo || echo /b 1
+call :build_test_module filesystem_tests || exit /b 1
+call :build_test_module tasks_tests || exit /b 1
+call :test_module paltool || exit /b 1
+exit /b
+
+:test_module
+set Module=%1
+ion -check -os osx %Module% || exit /b 1
+ion -check %Module% || exit /b 1
+exit /b
+
+:build_test_module
+set Module=%1
+ion -check -os osx %Module% || exit /b 1
+set IONC=out_%Module%.c
+ion -o %IONC% %Module% || exit /b 1
+set O="%Module%.exe"
+cl "%IONC%" /Fe:"%O%" %CLFlags% -nologo || echo /b 1
 echo PROGRAM	%O%	TEST
-
-ion tasks_tests || exit /b 1
-set O="tasks_tests.exe"
-cl out_tasks_tests.c /Fe:"%O%" %CLFlags% -nologo || echo /b 1
-echo PROGRAM	%O%	TEST
-
-
+exit /b
