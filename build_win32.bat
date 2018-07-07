ff7c97caf33906c4d56e80c5ef4fec70b8f0a8c1
nicolas <nicolas@uucidl.com>
2018-07-07 11:49:02 +0200

    Factor out registry

--- build_win32.bat
+++ build_win32.bat
@@ -1,9 +1,10 @@
+@echo off
 setlocal
 set CLFlags=%CLFLAGS%
 
 ion paltool || exit /b 1
 
 set O="paltool.exe"
-cl out_paltool.c /Fe:"%O%" %CLFlags% || echo /b 1
+cl out_paltool.c /Fe:"%O%" %CLFlags% -nologo || echo /b 1
 echo PROGRAM	%O%
 
