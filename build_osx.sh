274b89a8ffac72ff615cabc26559bfdb749c5a45
Nicolas Léveillé <nicolas@uucidl.com>
2018-08-16 10:27:10 +0200

    Primitive task dependency on OSX

--- build_osx.sh
+++ build_osx.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 set -e
+HERE="$(dirname "${0}")"
+if [[ -z "${OUTPUT_DIR}" ]] ; then OUTPUT_DIR="${HERE}"/output; fi
 CFLAGS="${CFLAGS}"
 
-HERE="$(dirname "${0}")"
+[[ -d "${OUTPUT_DIR}" ]] || mkdir -p "${OUTPUT_DIR}"
+
+C="${OUTPUT_DIR}"/out_paltool_osx_x64.c
 pushd "${HERE}"
-ion -os osx -arch x64 paltool
+ion -os osx -arch x64 -o "$C" paltool
 popd
 
 O="${HERE}/paltool.elf"
-cc "${HERE}"/out_paltool.c -o "${O}" $CFLAGS && printf "PROGRAM\t%s\n" "${O}"
+cc "${C}" -o "${O}" $CFLAGS && printf "PROGRAM\t%s\n" "${O}"
 
