27235983c5f796aa874e231916a40bbb2071931f
nicolas <nicolas@uucidl.com>
2019-02-25 09:41:29 +0100

    Run tests systematically.

--- tests_win32.bat
+++ tests_win32.bat
@@ -18,7 +18,9 @@ if %errorlevel% neq 0 exit /b 1
 call :build_test_module containers_tests || exit /b 1
 call :build_test_module filesystem_tests || exit /b 1
 call :build_test_module tasks_tests || exit /b 1
+
 call :test_module paltool || exit /b 1
+
 exit /b
 
 :test_module
@@ -33,6 +35,7 @@ ion -check -os osx %Module% || exit /b 1
 set IONC="%ObjDir%\out_%Module%.c"
 ion -o "%IONC%" %Module% || exit /b 1
 set O="%OutputDir%\%Module%.exe"
-cl "%IONC%" -Fe:"%O%" -Fo:"%ObjDir%"\ %CLFlags% -nologo || echo /b 1
+cl "%IONC%" -Fe:"%O%" -Fo:"%ObjDir%"\ %CLFlags% -nologo || exit /b 1
 echo PROGRAM	%O%	TEST
+%O% || exit /b 1
 exit /b
